031686d30ac0e452212e3c7639e3f92a334dbcd6
Sebastian <sebastianlopez1935@gmail.com>
2023-07-08 12:31:53 -0700

    nuevo commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const lineasContainer = document.querySelector('.lineas-container');
+const lineas = document.querySelectorAll('.linea');
+let lineasExtendidas = false;
+let opacidad = false;
+const h1 = document.querySelector('h1'); // Seleccionar el elemento h1
+
+lineasContainer.addEventListener('mouseenter', extendLines);
+lineasContainer.addEventListener('mouseleave', resetLines);
+
+function extendLines() {
+    lineas.forEach((linea) => {
+        linea.style.transform = 'translateX(0)';
+    });
+    lineasExtendidas = true;
+    opacidad = true;
+
+    setTimeout(() => {
+        h1.style.opacity = 1;
+    }, 3000); // Retrasar la aplicación del cambio de opacidad en 500 milisegundos (ajusta el valor según sea necesario)
+}
+
+function resetLines() {
+    if (!lineasExtendidas && opacidad) {
+        return;
+    } 
+    lineas.forEach((linea) => {
+        linea.style.transform = 'translateX(0)';
+    });
+    lineasExtendidas = false;
+}
